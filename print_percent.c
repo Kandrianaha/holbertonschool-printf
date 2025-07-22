d36f4c33d1dceaa2e82daab4a2f569d6ca489c02
Kandrianaha <k.andria@icloud.com>
2025-07-22 07:58:33 +0000

    print_string and print_percent

--- print_percent.c
+++ print_percent.c
@@ -1,1 +1,15 @@
-placeholder
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_percent - prints a percent sign
+ * @args: va_list 
+ * Return: Number of characters printed
+ */
+int print_percent(va_list args)
+{
+	(void)args;
+
+	write(1, "%", 1);
+	return (1);
+}
--- print_string.c
+++ print_string.c
@@ -1,1 +1,27 @@
-placeholder
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_string - prints a string
+ * @args: va_list containing the string to print
+ * Return: Number of characters printed
+ */
+int print_string(va_list args)
+{
+	char *s;
+	int count = 0;
+
+	s = va_arg(args, char *);
+
+	if (s == NULL)
+	s = "(null)";
+
+	while (*s)
+	{
+	write(1, s, 1);
+	s++;
+	count++;
+	}
+
+	return (count);
+}	
