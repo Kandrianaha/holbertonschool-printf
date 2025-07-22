a673e9f3b4b1891c30b8a0b1ead48d7a555297fb
ashleigh6734 <ashleigh6734@gmail.com>
2025-07-22 07:06:35 +0000

    enter code

--- print_char.c
+++ print_char.c
@@ -1,1 +1,17 @@
-placeholder
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_char - Prints a single character
+ * @args: va_list containing the character to print
+ *
+ * Return: Number of characters printed (always 1)
+ */
+
+int print_char(va_list args)
+{
+	char c;
+
+	c = va_arg(args, int);
+	return (write(1, &c, 1)); /* write the char to stdout */
+}
