6c9c55ad0c27c8f42d6cfc0c7d6811be05b93dfc
ashleigh6734 <ashleigh6734@gmail.com>
2025-07-22 06:58:21 +0000

    adding prototypes

--- main.h
+++ main.h
@@ -1,1 +1,20 @@
-placeholder
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+typedef struct conver
+{
+	char *sym;
+	int (*handler)(va_list);
+} conver_t;
+
+/* prototypes */
+int _printf(const char *format, ...);
+int parse_format(const char *format, conver_t f_list[], va_list args);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
+int print_int(va_list args);
+
+#endif
