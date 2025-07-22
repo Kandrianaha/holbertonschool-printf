2417cfac4131d7e8d408b895d54a7e972af81620
tranbnn <tranbn@outlook.com>
2025-07-22 16:00:42 +0000

    print_int code

--- print_int.c
+++ print_int.c
@@ -1,1 +1,40 @@
-placeholder
+#include "main.h"
+#include <unistd.h>
+#include <stdarg.h>
+
+/**
+ * print_num - prints int number
+ * @n: the int number
+ * Return: int
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar ('-');
+		n = -n;
+	}
+
+	if (n /10) /* checks for more than one digit */
+	{
+		print_number(n / 10);
+	}
+
+	_putchar((n % 10) + '0');
+}
+
+/**
+ * print_int - print integers
+ * @args: the variable list with integer to print
+ * Return: print number of characters
+ */
+
+int print_int(va_list args)
+{
+	int num;
+
+	num = va_arg(args, int);
+	print_number(num);
+	return (1);
+}
