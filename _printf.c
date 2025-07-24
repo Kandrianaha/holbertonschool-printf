6d8bca0c793b31c7195e5da8ed213d0115e50f1e
tranbnn <tranbn@outlook.com>
2025-07-24 06:20:58 +0000

    betty

--- _printf.c
+++ _printf.c
@@ -9,24 +9,26 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int printed_chars;
-
-    conver_t format_list[] = {
-        {"c", print_char},
-        {"s", print_string},
-        {"%", print_percent},
-	{"d", print_int},
-	{"i", print_int},
-        {NULL, NULL}
-    };
-
-    if (format == NULL)
-        return (-1);
-
-    va_start(args, format);
-    printed_chars = parse_format(format, format_list, args);
-    va_end(args);
-
-    return (printed_chars);
+	va_list args;
+	int printed_chars;
+	
+	conver_t format_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_int},
+		{"i", print_int},
+		{NULL, NULL}
+	};
+	
+	if (format == NULL)
+	{
+		return (-1);
+	}
+	
+	va_start(args, format);
+	printed_chars = parse_format(format, format_list, args);
+	va_end(args);
+	
+	return (printed_chars);
 }
