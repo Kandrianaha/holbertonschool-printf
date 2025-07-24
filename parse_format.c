6554627876a4e7441768b44321eb1ae6e4411c12
tranbnn <tranbn@outlook.com>
2025-07-24 06:25:08 +0000

    fix

--- parse_format.c
+++ parse_format.c
@@ -10,41 +10,41 @@
  */
 int parse_format(const char *format, conver_t f_list[], va_list args)
 {
-    int i = 0, j, printed_chars = 0;
-
-    while (format[i] != '\0')
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            if (format[i] == '\0')
-                return (-1);
-
-            for (j = 0; f_list[j].sym != NULL; j++)
-            {
-                if (format[i] == *(f_list[j].sym))
-                {
-                    printed_chars += f_list[j].handler(args);
-                    break;
-                }
-            }
-
-            if (f_list[j].sym == NULL)
-            {
-                write(1, "%", 1);
-                write(1, &format[i], 1);
-                printed_chars += 2;
-            }
-        }
-        else
-        {
-            write(1, &format[i], 1);
-            printed_chars++;
-        }
-
-        i++;
-    }
-
-    return (printed_chars);
+	int i = 0, j, printed_chars = 0;
+	
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+	{
+		i++;
+		if (format[i] == '\0')
+		{
+			return (-1);
+		}
+		
+		for (j = 0; f_list[j].sym != NULL; j++)
+		{
+			if (format[i] == *(f_list[j].sym))
+			{
+				printed_chars += f_list[j].handler(args);
+				break;
+			}
+		}
+		
+		if (f_list[j].sym == NULL)
+		{
+			write(1, "%", 1);
+			write(1, &format[i], 1);
+			printed_chars += 2;
+		}
+	}
+		else
+		{
+			write(1, &format[i], 1);
+			printed_chars++;
+		}
+		
+		i++;
+	}
+	return (printed_chars);
 }
-
